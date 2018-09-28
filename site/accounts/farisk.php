bd32d269681c05453fa63a60bbc5a3c4bcf36764
fariskas <faris.kassim44@gmail.comgit config --global user.name Your>
2018-09-28 15:12:50 +0900

    first commit

---
+++ site/accounts/farisk.php
@@ -0,0 +1,14 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: farisk
+password: >
+  $2y$10$CsoQoVx3qpkKkyXQy4V.xOj.Z3T3cuD1q5dhA9IpTPOQUCv3Rietq
+email: faris_kassim@hotmail.com
+language: en
+role: admin
+history:
+  - websites/website-b
+  - websites/website-a
+  - websites/website-e
+  - websites/website-d
+  - websites/website-c
